85ba881af08636f0943e9d27d9a98e8c4e21672f
pjs <pjs@alum.mit.edu>
2017-06-06 14:23:34 -0700

    Removed unneeded \ in print(), added seeding

--- MMk.py
+++ MMk.py
@@ -47,9 +47,10 @@ class MMk(SimpleKit):
 
     def dumpState(self, event):
         """Dump of the current state of the model."""
-        print("Time: %6.2f" % self.model_time, "  Event: %-12s" % event, \
-              "  Queue Length: %3d" % self.qLength, " Available Servers: ", \
+        print("Time: %6.2f" % self.model_time, "  Event: %-12s" % event,
+              "  Queue Length: %3d" % self.qLength, " Available Servers: ",
               self.numAvailableServers)
 
 if __name__ == '__main__':
+    numpy.random.seed(12345)
     MMk(4.5, 1.0, 5).run()      # Instantiate and run a copy of the MMk model.
