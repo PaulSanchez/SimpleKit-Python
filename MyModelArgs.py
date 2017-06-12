a1e4863eb112992b8eb6fc83cd493c76fd7b8d48
pjs <pjs@alum.mit.edu>
2017-06-12 16:27:45 -0700

    comment code, remove extraneous multiplication of random value

--- MyModelArgs.py
+++ MyModelArgs.py
@@ -3,13 +3,17 @@ import numpy.random as rnd
 import math
 
 class MyModel(SimpleKit):
+    """
+    Silly model keeps doubling its state until simulated time exceeds 10.
+    Along the way it demonstrates argument passing for events & priorities.
+    """
     def init(self):
         self.x = 1
         self.schedule(self.increment, rnd.randint(2), 1, ord('a'), priority = 2)
 
     def increment(self, n, c):
         self.x += n
-        self.schedule(self.increment, 2.0 * rnd.randint(2), self.x, c + 1)
+        self.schedule(self.increment, rnd.randint(2), self.x, c + 1)
         print(self.model_time, self.x, chr(c))
         if self.model_time > 10:
             self.schedule(self.halt, 0.0, priority = 1)
