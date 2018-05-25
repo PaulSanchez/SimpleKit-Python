6a5d39dd0021a2cac277d04c84b74723d1567629
pjs <pjs@alum.mit.edu>
2018-05-25 11:14:56 -0700

    Revert MMk to pure MMk for demo purposes

--- MMk.py
+++ MMk.py
@@ -7,29 +7,21 @@ import sys
 class MMk(SimpleKit):
     """Implementation of an M/M/k queueing model using SimpleKit."""
 
-    def __init__(self, arrivalRate, serviceRate, maxServers):
+    def __init__(self, arrivalRate, serviceRate, maxServers, shutdownTime):
         """Construct an instance of the M/M/k."""
         SimpleKit.__init__(self)
         self.meanArrival = 1.0 / arrivalRate
         self.meanSvc = 1.0 / serviceRate
         self.maxServers = maxServers
-        self.qLength = 0
-        self.numAvailableServers = 0
+        self.shutdownTime = shutdownTime
 
     def init(self):
         """Initialize all state variables, schedule first arrival and halt."""
         self.numAvailableServers = self.maxServers
         self.qLength = 0
         self.schedule(self.arrival, 0.0)
-        self.schedule(self.arrival, 80.0)
-        self.schedule(self.shutdown, 100.0, priority = 0)
-        self.schedule(self.restart, 105.0)
-        self.schedule(self.shutdown, 130.0, priority = 0)
+        self.schedule(self.shutdown, self.shutdownTime, priority = 0)
         self.dumpState("Init")
-
-    def restart(self):
-        self.schedule(self.arrival, 0.0)
-        self.dumpState("restart")
 
     def arrival(self):
         """Increment queue, schedule next arrival, beginService if possible."""
@@ -55,10 +47,7 @@ class MMk(SimpleKit):
 
     def shutdown(self):
         """Close shop by shutting doors, i.e., no more arrivals."""
-        if self.model_time < 110.0:
-            self.cancel_all(self.arrival)
-        else:
-            self.cancel_next(self.arrival)
+        self.cancel_next(self.arrival)
         self.dumpState("shutdown")
 
     def dumpState(self, event):
@@ -69,5 +58,16 @@ class MMk(SimpleKit):
 
 if __name__ == '__main__':
     numpy.random.seed(12345)
-    # Recommend running with 4.5, 1.0, 5
-    MMk(float(sys.argv[1]), float(sys.argv[2]), int(sys.argv[3])).run()      # Instantiate and run a copy of the MMk model.
+    # Instantiate and run a copy of the MMk model.
+    if len(sys.argv) == 1:
+        model = MMk(4.5, 1.0, 5, 100.0).run()
+    elif len(sys.argv) == 5:
+        model = MMk(float(sys.argv[1]), float(sys.argv[2]),
+                    int(sys.argv[3]), float(sys.argv[4])).run()
+    else:
+        m1 = "Please specify arrival rate, per-server service rate, # servers,"
+        m2 = "and shutdown time separated by spaces on the command-line."
+        m3 = "If no arguments are given, these default to: 4.5 1.0 5 100.0"
+        print("\n\t" + m1, file = sys.stderr)
+        print("\t" + m2, file = sys.stderr)
+        print("\n\t" + m3 + "\n", file = sys.stderr)
